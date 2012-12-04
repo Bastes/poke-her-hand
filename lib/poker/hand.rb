55a95f735de1f26508a5b98a2e42f7cdb6ac484b
Michel Belleville <mib@octo.com>
2012-12-04 15:17:25 +0100

    Basic hand sorting

--- lib/poker/hand.rb
+++ lib/poker/hand.rb
@@ -1,4 +1,18 @@
 module Poker
   class Hand
+    VALUES = (2..10).
+      inject({}) { |r, i| r.tap { r["#{i}"] = i } }.
+      merge({ 'J' => 11, 'Q' => 12, 'K' => 13, 'A' => 14 })
+    VALUE = /\A([2-9JQKA]|10)/
+
+    def initialize(cards)
+      @cards = cards.split(/ /).
+        sort { |a, b| [VALUES[a[VALUE]], a] <=> [VALUES[b[VALUE]], b] }.
+        join ' '
+    end
+
+    def to_s
+      @cards
+    end
   end
 end
--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Poker::Hand do
-  pending "write it"
+  {
+    '10D 2S 2C KH QD' => '2C 2S 10D QD KH',
+    '10D 10C 10S 10H 2S' => '2S 10C 10D 10H 10S',
+    'AD QD AC QC JH' => 'JH QC QD AC AD'
+  }.each do |given, expected|
+    context "given these cards: #{given}" do
+      subject { Poker::Hand.new(given) }
+
+      its(:to_s) { should == expected }
+    end
+  end
 end
