59edef9587f97beae4cc80e90d1498057c212c31
Michel Belleville <mib@octo.com>
2012-12-04 18:30:52 +0100

    Requires a proper poker hand

--- lib/poker/hand.rb
+++ lib/poker/hand.rb
@@ -1,11 +1,13 @@
 module Poker
   class Hand
+    ACCEPTABLE = /\A(([2-9JQKA]|10)[CDHS])( ([2-9JQKA]|10)[CDHS]){4}\Z/
     VALUES = (2..10).
       inject({}) { |r, i| r.tap { r["#{i}"] = i } }.
       merge({ 'J' => 11, 'Q' => 12, 'K' => 13, 'A' => 14 })
     VALUE = /\A([2-9JQKA]|10)/
 
     def initialize(cards)
+      raise ArgumentError.new("#{cards} is not a poker hand") unless cards =~ ACCEPTABLE
       @cards = cards.split(/ /).
         sort { |a, b| [VALUES[a[VALUE]], a] <=> [VALUES[b[VALUE]], b] }.
         join ' '
--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -1,6 +1,18 @@
 require 'spec_helper'
 
 describe Poker::Hand do
+  [
+    'garbage',
+    '2H 2D 2S 2C',
+    '7C 8C 9C 10H JS QH',
+    '11D 3C QS KC AH',
+    '8H 9G 3C 7D KH'
+  ].each do |garbage|
+    context "given #{garbage}" do
+      it { expect { Poker::Hand.new(garbage) }.to raise_error ArgumentError }
+    end
+  end
+
   {
     '10D 2S 2C KH QD' => '2C 2S 10D QD KH',
     '10D 10C 10S 10H 2S' => '2S 10C 10D 10H 10S',
